12dfed4a4b3f9cce53f132031a3b5da693a3b1c5
iWas-Coder <wasymatieh01@gmail.com>
2022-07-22 01:28:54 +0200

    Project init

---
+++ main.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+const { app, BrowserWindow } = require('electron')
+
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    win.loadFile('index.html')
+}
+
+
+app.whenReady().then(() => {
+    createWindow()
+    app.commandLine.appendSwitch('disable-gpu-sandbox')
+})
