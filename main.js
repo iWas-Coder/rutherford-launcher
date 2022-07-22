ac9beaddaea71f181a9d0b25e5066b9d01b1d17a
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-07-22 12:53:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 // @ts-check
 
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 
 const createWindow = () => {
     const win = new BrowserWindow({
+        title: 'Rutherford Launcher',
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'app/preload.js'),
+            nodeIntegration: true
+        },
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#f7df1e',
+            symbolColor: '#000000'
+        }
     })
-    win.loadFile('app/index.html')
+    win.loadURL(path.join(__dirname, 'app/index.html'))
 }
 
 
 app.whenReady().then(() => {
     createWindow()
-    app.commandLine.appendSwitch('in-process-gpu')
+    // app.commandLine.appendSwitch('in-process-gpu')
+    
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
