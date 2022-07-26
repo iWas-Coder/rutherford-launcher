c0d7109cc9df1ce2db55e65ff2015507a8728409
iWas-Coder <wasymatieh01@gmail.com>
2022-07-26 02:10:51 +0200

    Developing app (fixed executing batch scripts)

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,12 @@
 // @ts-check
+
+
+const { shell } = require('electron')
+
+const launchMC = () => {    
+    shell.beep()
+    shell.openPath('D:\\MINECRAFT\\bin\\script\\funcs\\play.bat')
+        .then(err => {
+            console.error(err)
+        })
+}
